c690a340aa92c4509b4e2ee76c03b812928397f0
rajushank84 <rajushank84@yahoo.com>
2025-11-29 19:19:17 +0530

    Adding files

---
+++ data.js
@@ -0,0 +1,473 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.0206618079289242,
+        "pitch": 0.042150558105632285,
+        "fov": 1.426679470426884
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9188139988495383,
+          "pitch": 0.6736810910011108,
+          "rotation": 0,
+          "target": "1-hall---near-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hall---near-entrance",
+      "name": "Hall - near entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.2957554364386361,
+        "pitch": 0.22735834176031489,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4725632091680056,
+          "pitch": 0.6179622637260795,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 0.89480927428726,
+          "pitch": 0.5667270698704705,
+          "rotation": 7.0685834705770345,
+          "target": "2-hall---tv-area"
+        },
+        {
+          "yaw": -1.3710617534535139,
+          "pitch": 0.5284742381320964,
+          "rotation": 5.497787143782138,
+          "target": "4-hall---pooja-area"
+        },
+        {
+          "yaw": -0.9937643786308339,
+          "pitch": 0.4055074088720474,
+          "rotation": 0.7853981633974483,
+          "target": "7-bedroom"
+        },
+        {
+          "yaw": -1.6136997753851325,
+          "pitch": 0.4461299261874867,
+          "rotation": 4.71238898038469,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 0.33407046462784074,
+          "pitch": 0.3973897669287467,
+          "rotation": 0,
+          "target": "8-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hall---tv-area",
+      "name": "Hall - TV area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.14292521763153943,
+        "pitch": 0.047061104099409334,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15395848676874202,
+          "pitch": 0.504716484160106,
+          "rotation": 0,
+          "target": "3-balcony"
+        },
+        {
+          "yaw": 2.953445533830795,
+          "pitch": 0.26180754622113334,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 2.466500312427023,
+          "pitch": 0.3847186169734087,
+          "rotation": 5.497787143782138,
+          "target": "8-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.096733620175388,
+        "pitch": 0.2070779303240755,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05417244608050531,
+          "pitch": 0.8765017883927229,
+          "rotation": 0,
+          "target": "2-hall---tv-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hall---pooja-area",
+      "name": "Hall - pooja area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.486203507244274,
+        "pitch": 0.18047540373746784,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3094416923242225,
+          "pitch": 0.4237309497110644,
+          "rotation": 0.7853981633974483,
+          "target": "1-hall---near-entrance"
+        },
+        {
+          "yaw": -0.4801966863964733,
+          "pitch": 0.3895704997978413,
+          "rotation": 5.497787143782138,
+          "target": "2-hall---tv-area"
+        },
+        {
+          "yaw": 1.6225530703802864,
+          "pitch": 0.4669146421741832,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 2.5305013177920026,
+          "pitch": 0.5660725524212431,
+          "rotation": 0.7853981633974483,
+          "target": "6-bathroom"
+        },
+        {
+          "yaw": -1.2250642628055566,
+          "pitch": 0.5772628885747597,
+          "rotation": 0,
+          "target": "7-bedroom"
+        },
+        {
+          "yaw": -0.038105503324894485,
+          "pitch": 0.23286987971163597,
+          "rotation": 0,
+          "target": "8-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.0214860247630853,
+        "pitch": 0.10635150519417991,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7917285460754808,
+          "pitch": 0.5664222217222097,
+          "rotation": 0,
+          "target": "4-hall---pooja-area"
+        },
+        {
+          "yaw": -0.7893771614053744,
+          "pitch": 0.18603401556920574,
+          "rotation": 0,
+          "target": "2-hall---tv-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.082968842717815,
+        "pitch": -0.0675727983162897,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5317304449334941,
+          "pitch": 0.6813204275802729,
+          "rotation": 5.497787143782138,
+          "target": "4-hall---pooja-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.8616886141223183,
+        "pitch": 0.19272218081222903,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9489462635550581,
+          "pitch": 0.39628581420004494,
+          "rotation": 0,
+          "target": "4-hall---pooja-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-office",
+      "name": "Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.0126185722503713,
+        "pitch": 0.2858035206406875,
+        "fov": 1.4822420275829202
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49290635596956633,
+          "pitch": 0.398763421841938,
+          "rotation": 5.497787143782138,
+          "target": "1-hall---near-entrance"
+        },
+        {
+          "yaw": -0.23865947608708638,
+          "pitch": 0.2426223259499345,
+          "rotation": 0.7853981633974483,
+          "target": "2-hall---tv-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
